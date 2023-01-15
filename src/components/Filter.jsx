4f5849440df94aabc2e600bdc2fc4103cacf10ea
Sushil1204 <sushilspundkar@gmail.com>
2023-01-15 18:35:06 +0530

    filter component modified

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,52 +5,39 @@ import { filterClicked, openEmail } from "../action";
 const Filter = () => {
   const dispatch = useDispatch();
 
-  const addSelectedClass = (e) => {
-    let element = document.getElementById("filter-btn");
-    for (let i = 0; i < element.length; i++) {
-      element[i].classList.remove("selected");
-    }
-    e.target.classList.add("selected");
-  };
   return (
     <section className="flex font-normal items-center">
       <div className="text-md">
         <span>Filter By :</span>
       </div>
       <div className="flex space-x-10 ml-20">
-        <span
-        id="filter-btn"
-          className="filter-btn hover:bg-[#CFD2DC] rounded-xl px-6 cursor-pointer border-2 border-solid transparent"
-          onClick={(e) => {
+        <button
+          className="hover:bg-[#CFD2DC] active:bg-[#CFD2DC] focus:outline-none focus:ring focus:ring-gray-600 rounded-xl px-6 cursor-pointer border-2 border-solid transparent"
+          onClick={() => {
             dispatch(filterClicked("Unread"));
             dispatch(openEmail(null));
-            addSelectedClass(e);
           }}
         >
           Uread
-        </span>
-        <span
-        id="filter-btn"
-          className="filter-btn hover:bg-[#CFD2DC] rounded-xl px-6 cursor-pointer border-2 border-solid transparent"
-          onClick={(e) => {
+        </button>
+        <button
+          className="hover:bg-[#CFD2DC] active:bg-[#CFD2DC] focus:outline-none focus:ring focus:ring-gray-600 rounded-xl px-6 cursor-pointer border-2 border-solid transparent"
+          onClick={() => {
             dispatch(filterClicked("Read"));
             dispatch(openEmail(null));
-            addSelectedClass(e);
           }}
         >
           Read
-        </span>
-        <span
-        id="filter-btn"
-          className="filter-btn hover:bg-[#CFD2DC] rounded-xl px-6 cursor-pointer border-2 border-solid transparent"
-          onClick={(e) => {
+        </button>
+        <button
+          className="hover:bg-[#CFD2DC] active:bg-[#CFD2DC] focus:outline-none focus:ring focus:ring-gray-600 rounded-xl px-6 cursor-pointer border-2 border-solid transparent"
+          onClick={() => {
             dispatch(filterClicked("Favorites"));
             dispatch(openEmail(null));
-            addSelectedClass(e);
           }}
         >
           Favorites
-        </span>
+        </button>
       </div>
     </section>
   );
